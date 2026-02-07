e7347e087368727aface896b714e23725dd0b350
jibachh <subhamchaudhary199@gmail.com>
2026-02-08 02:36:28 +0545

    adding faqs page and some more content in the child pages

--- src/main.jsx
+++ src/main.jsx
@@ -4,19 +4,34 @@ import App from "./App.jsx";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import SearchPermit from "./pages/SearchPermit.jsx";
 import Home from "./pages/Home.jsx";
+import Register from "./pages/Register.jsx";
+import Login from "./pages/Login.jsx";
+import News from "./pages/News.jsx";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
     children: [
-       {
+      {
         index: true,
-        element: ( <Home />),
+        element: <Home />,
       },
       {
-        path: "searchpermits",
+        path: "/register",
+        element: <Register />,
+      },
+      {
+        path: "/login",
+        element: <Login />,
+      },
+      {
+        path: "/searchpermits",
         element: <SearchPermit />,
+      },
+      {
+        path: "/news",
+        element: <News />,
       },
     ],
   },
